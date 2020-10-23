a20345bd85501840c4f7a935010a2eca2728c606
Luke Anderson <landerson61@gmail.com>
2020-10-23 17:11:54 +0100

    Add user registration & signin

--- src/config.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const env = process.env.NODE_ENV || 'development';
-const dbConfigFile = path.join(__dirname, `./${env}DBConfig.json`);
-exports.parsedConfig = JSON.parse(fs.readFileSync(dbConfigFile, 'UTF-8'));
